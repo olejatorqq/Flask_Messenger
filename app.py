992b916156bdb0a8cc2b05c64ea2b199cc419066
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-03-22 22:07:57 +0300

    Added head html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():
-    return "About page"
+    return render_template("about.html")
 
 
 @app.route('/user/<string:name>/<int:id>')
