3cf22da907c9863eeebba5c171193b7a51d55adb
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-03-22 22:46:18 +0300

    Added form for chatting, added placeholders

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -21,14 +22,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/create-chat')
+def create_chat():
+    return render_template("create-chat.html")
 
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User page: " + name + " - " + str(id)
 
 
 
