f5cf0a6b458a73e5cd820ef1204695c99bcb23c8
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-03-22 22:27:31 +0300

    Added db and db class

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+db = SQLAlchemy(app)
+
+
+class Text(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return '<Text %r>' % self.id
 
 
 @app.route('/')
